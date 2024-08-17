2cad6d76db323407885e1170b0dc069db1a6de80
Matt Lewis <matt@mplewis.com>
2024-08-17 16:01:59 -0600

    feat: wiggle tools when grow/shrink

--- scripts/game.gd
+++ scripts/game.gd
@@ -8,6 +8,7 @@ const BELT_SPEED = 20  # px/tick
 const SCREEN_CENTER_X = SCREEN_WIDTH_PX * 1.0 / 2
 const CENTER_WAIT_SEC = 3.0  # seconds to wait in the middle
 const GROW_SHRINK_RATE = 0.02  # scale factor per tick
+const GROW_SHAKE_MAX_ROT = 10  # degrees in either direction to wiggle the tool
 const CLOTHING_MAX_SCALE = 2.0
 const CLOTHING_MIN_SCALE = 0.5
 
@@ -16,7 +17,8 @@ var stop_waiting_at: float = 0
 
 @onready var current_animal: Node2D = $CurrentAnimal
 @onready var current_clothing: Node2D = $CurrentClothing
-# @onready var alpaca_shirt: Node2D = $AlpacaShirt
+@onready var grow_tool: Node2D = $GrowTool
+@onready var shrink_tool: Node2D = $ShrinkTool
 
 
 func _ready():
@@ -50,9 +52,16 @@ func grow_shrink_clothing():
 	if Input.is_action_pressed("ui_grow"):
 		if current_clothing.scale.x < CLOTHING_MAX_SCALE:
 			current_clothing.scale += Vector2(GROW_SHRINK_RATE, GROW_SHRINK_RATE)
-	elif Input.is_action_pressed("ui_shrink"):
+			grow_tool.rotation_degrees = randf_range(-GROW_SHAKE_MAX_ROT, GROW_SHAKE_MAX_ROT)
+	else:
+		grow_tool.rotation_degrees = 0
+
+	if Input.is_action_pressed("ui_shrink"):
 		if current_clothing.scale.x > CLOTHING_MIN_SCALE:
 			current_clothing.scale -= Vector2(GROW_SHRINK_RATE, GROW_SHRINK_RATE)
+			shrink_tool.rotation_degrees = randf_range(-GROW_SHAKE_MAX_ROT, GROW_SHAKE_MAX_ROT)
+		else:
+			shrink_tool.rotation_degrees = 0
 
 
 func _process(_delta):
