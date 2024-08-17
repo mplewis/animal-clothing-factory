a9801da7e943c0f5a362ab0e108eaa194038019d
Matt Lewis <matt@mplewis.com>
2024-08-17 16:33:31 -0600

    feat: alpaca wears resized clothing

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,25 +1,44 @@
 extends Node2D
 
-enum BeltState { MOVE_ANIMAL_IN, WAITING, MOVE_ANIMAL_OUT }
+enum PlayState { MOVE_ANIMAL_IN, SIZING_CLOTHING, WEARING_CLOTHING, MOVE_ANIMAL_OUT }
 
+## Width of the game window, in pixels
 const SCREEN_WIDTH_PX = 1920
+## How far off-screen to start/end the animal motion, in pixels
 const OFF_SCREEN_DISTANCE_PX = 300
-const BELT_SPEED = 20  # px/tick
+## Speed of belt that moves animals in/out, in px/tick
+const BELT_SPEED = 20
+## The X coord for the center of the screen. This is where the animal stops.
 const SCREEN_CENTER_X = SCREEN_WIDTH_PX * 1.0 / 2
-const CENTER_WAIT_SEC = 3.0  # seconds to wait in the middle
-const GROW_SHRINK_RATE = 0.02  # scale factor per tick
-const GROW_SHAKE_MAX_ROT = 10  # degrees in either direction to wiggle the tool
+## How fast to grow/shrink clothing when the player holds the button
+const GROW_SHRINK_RATE = 0.02
+## degrees in either direction to wiggle the tool during a grow/shrink operation
+const GROW_SHAKE_MAX_ROT = 10
+## Max scale of clothing when growing
 const CLOTHING_MAX_SCALE = 2.0
+## Min scale of clothing when shrinking
 const CLOTHING_MIN_SCALE = 0.5
 
-var belt_state: BeltState = BeltState.MOVE_ANIMAL_IN
+## The current state of the interactive player elements
+var play_state: PlayState = PlayState.MOVE_ANIMAL_IN
+## If true, the player is currently holding down a "resize" key
 var resizing: bool = false
+## The player's score
 var score: int = 0
 
+## The current animal to dress
 @onready var current_animal: Node2D = $CurrentAnimal
+## Where the shirt sits on the alpaca
+@onready var alpaca_clothing_anchor: Node2D = $CurrentAnimal/Alpaca/ClothingAnchor
+## Where clothing sits during sizing
+@onready var new_clothing_anchor: Node2D = $NewClothingAnchor
+## The current clothing to resize
 @onready var current_clothing: Node2D = $CurrentClothing
+## The tool used to grow clothing
 @onready var grow_tool: Node2D = $GrowTool
+## The tool used to shrink clothing
 @onready var shrink_tool: Node2D = $ShrinkTool
+## The label showing the player's score
 @onready var score_label: Label = $UI/StarLabel/ScoreLabel
 
 
@@ -29,12 +48,13 @@ func _ready():
 
 func set_animal_to_start():
 	current_animal.position.x = -OFF_SCREEN_DISTANCE_PX
-	belt_state = BeltState.MOVE_ANIMAL_IN
+	play_state = PlayState.MOVE_ANIMAL_IN
 	reset_clothing()
 
 
 func reset_clothing():
 	current_clothing.scale = Vector2(1, 1)
+	current_clothing.position = new_clothing_anchor.position
 
 
 func incr_score(points: int):
@@ -43,23 +63,20 @@ func incr_score(points: int):
 
 
 func move_animal_on_belt():
-	match belt_state:
-		BeltState.MOVE_ANIMAL_IN:
+	match play_state:
+		PlayState.MOVE_ANIMAL_IN:
 			current_animal.position.x += BELT_SPEED
 			if current_animal.position.x > SCREEN_CENTER_X:
-				belt_state = BeltState.WAITING
+				play_state = PlayState.SIZING_CLOTHING
 
-		BeltState.WAITING:
-			pass
-
-		BeltState.MOVE_ANIMAL_OUT:
+		PlayState.MOVE_ANIMAL_OUT:
 			current_animal.position.x += BELT_SPEED
 			if current_animal.position.x > SCREEN_WIDTH_PX + OFF_SCREEN_DISTANCE_PX:
 				set_animal_to_start()
 
 
 func grow_shrink_clothing():
-	if belt_state != BeltState.WAITING:
+	if play_state != PlayState.SIZING_CLOTHING:
 		return
 
 	var grow = Input.is_action_pressed("ui_grow")
@@ -85,10 +102,22 @@ func grow_shrink_clothing():
 		resizing = false
 		grow_tool.rotation_degrees = 0
 		shrink_tool.rotation_degrees = 0
-		belt_state = BeltState.MOVE_ANIMAL_OUT
+		play_state = PlayState.WEARING_CLOTHING
 		incr_score(1)
+
+
+func wear_clothing():
+	if play_state == PlayState.WEARING_CLOTHING:
+		var goal = alpaca_clothing_anchor.global_position
+		current_clothing.position = current_clothing.position.lerp(goal, 0.1)
+		if current_clothing.position.distance_to(goal) < 1:
+			play_state = PlayState.MOVE_ANIMAL_OUT
+
+	elif play_state == PlayState.MOVE_ANIMAL_OUT:
+		current_clothing.position = alpaca_clothing_anchor.global_position
 
 
 func _process(_delta):
 	move_animal_on_belt()
 	grow_shrink_clothing()
+	wear_clothing()
