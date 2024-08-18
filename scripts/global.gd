183c44fb8d0436a03df315d0dd5b016c6debad76
Matt Lewis <matt@mplewis.com>
2024-08-18 15:46:07 -0600

    fix: print proper key debug

--- scripts/global.gd
+++ scripts/global.gd
@@ -7,4 +7,4 @@ var score := 0
 func print_many(desc: String, items: Dictionary) -> void:
 	print("%s:" % desc)
 	for key in items.keys():
-		print("    %s: %s" % [desc, items[key]])
+		print("    %s: %s" % [key, items[key]])
