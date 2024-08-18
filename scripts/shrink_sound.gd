b5a95fe4d1e6676810fa0cd1dd72604c389d2289
D0IP <69601196+D0IP@users.noreply.github.com>
2024-08-18 08:19:35 -0600

    shrink sound fully implemented

--- scripts/shrink_sound.gd
+++ scripts/shrink_sound.gd
@@ -1,9 +1,34 @@
 extends Node
 
+var volume_tween 
+var pitch_tween
 
 func _on_game_shrink_sound_start() -> void:
-	print("shrink sound start")
+	
+	## fade the volume in
+	if volume_tween:
+		volume_tween.kill() 
+	volume_tween = create_tween() 
+	volume_tween.tween_property($".", "volume_db", 0, 0.1) 
+	
+	## fade the pitch up
+	if pitch_tween:
+		pitch_tween.kill() 
+	pitch_tween = create_tween() 
+	pitch_tween.tween_property($".", "pitch_scale", 1, 0.4) 
 
 
 func _on_game_shrink_sound_stop() -> void:
-	print("shrink sound stop")
+	
+	## fade the volume out
+	if volume_tween:
+		volume_tween.kill() #stops tween if currenly running
+	volume_tween = create_tween() #create tween and assing to variable
+	volume_tween.tween_property($".", "volume_db", -80, 1) #ASP, Property, dBValue, time(seconds)
+	
+	## fade the pitch down
+	if pitch_tween:
+		pitch_tween.kill() 
+	pitch_tween = create_tween() 
+	pitch_tween.tween_property($".", "pitch_scale", 0, 1) 
+	
