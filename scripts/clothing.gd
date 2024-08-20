0aeb59e375f7f1c6cc845d9dc9eaa10daf234e06
Argent <10303636+ominocutherium@users.noreply.github.com>
2024-08-20 09:03:21 -0600

    Replace assets in clothing scene with atlas atlas

--- scripts/clothing.gd
+++ scripts/clothing.gd
@@ -4,6 +4,15 @@ extends Node2D
 @onready var sprite: Sprite2D = $Sprite
 
 
+func _ready() -> void:
+	if material is ShaderMaterial:
+		material = material.duplicate()
+		var inner_color: Color = Color(randf(),randf(),randf())
+		var lineart_color := inner_color.darkened(0.5)
+		(material as ShaderMaterial).set_shader_parameter("light_color", inner_color)
+		(material as ShaderMaterial).set_shader_parameter("dark_color", lineart_color)
+
+
 func _to_string():
 	return "<Clothing %s>" % self.name
 
